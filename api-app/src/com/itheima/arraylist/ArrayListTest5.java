bc07188275e52887edd0c4844e534e0a96aa3413
hyb2419805602 <2419805602@qq.com>
2022-04-04 00:00:08 +0800

    123456

--- api-app/src/com/itheima/arraylist/ArrayListTest5.java
+++ api-app/src/com/itheima/arraylist/ArrayListTest5.java
@@ -13,16 +13,19 @@ public class ArrayListTest5 {
     public static void main(String[] args) {
         // 目标：理解ArrayList集合存储自定义类型的变量。
         // 1. 定义电影类
+
         // 2. 创建三个电影对象
         Movie m1 = new Movie(  "《肖生克的救赎》", 9.7, "罗宾斯");
         Movie m2 = new Movie("《霸王别姬》", 9.6, "张国荣、张丰毅");
         Movie m3 = new Movie(  "《阿甘正传》", 9.5, "汤姆.汉克斯");
+
         // 3. 创建一个电影类型的ArrayList集合，存放三部电影。
         ArrayList<Movie> movies = new ArrayList<>();
         movies.add(m1);
         movies.add(m2);
         movies.add(m3);
         System.out.println(movies);
+
         // 4. 遍历电影类型的集合中的每个对象，访问它的信息即可
         for (int i = 0; i < movies.size(); i++) {
             Movie m = movies.get(i);
--- api-app/src/com/itheima/arraylist/ArrayListTest6.java
+++ api-app/src/com/itheima/arraylist/ArrayListTest6.java
@@ -1,4 +1,10 @@
 package com.itheima.arraylist;
 
+/**
+    案例：学生信息系统：展示数据，并按照学号完成搜索
+    学生类信息（学号，姓名，性别，班级）
+    测试数据：
+
+ */
 public class ArrayListTest6 {
 }
